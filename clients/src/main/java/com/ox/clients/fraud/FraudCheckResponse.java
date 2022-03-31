ba1c0ae848f835c419a8a42b5b759caacb97a5f7
wael hassan <wael.h@sybertechnology.com>
2022-03-31 10:25:50 +0200

    - add open feign module (clients) - add fraud interface as feign client

---
+++ clients/src/main/java/com/ox/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,15 @@
+package com.ox.clients.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FraudCheckResponse {
+
+    boolean isFraudster;
+}
