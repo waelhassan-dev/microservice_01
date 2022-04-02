2c4499d09d97f4497d1bde6b20ae48221039c548
wael hassan <wael.h@sybertechnology.com>
2022-04-02 19:32:03 +0200

    - testing publish messaging using commandline runner

--- notification/src/main/java/com/ox/notification/NotificationApplication.java
+++ notification/src/main/java/com/ox/notification/NotificationApplication.java
@@ -1,14 +1,31 @@
 package com.ox.notification;
 
+import com.ox.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.ox.notification", "com.ox.amqp"
+})
 @EnableEurekaClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+    //to test publishing message into rabbit mq
+    /*
+    @Bean
+    public CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+        return args -> {
+            producer.publish(new Person("wael", 27), notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+        };
+    }
+
+    record Person(String name, int age) {}
+     */
 }
